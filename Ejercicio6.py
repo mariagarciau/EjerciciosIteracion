2db0efda4aeaefa5dd03da1e06e9f9589f7938e4
mariagarciau <91331262+mariagarciau@users.noreply.github.com>
2022-02-21 12:47:08 +0000

    enunciado ej 6

---
+++ Ejercicio6.py
@@ -0,0 +1,8 @@
+"""Ejercicio 6: Historial de una cuenta corriente
+Se quiere conservar el historial de los movimientos mensuales en una cuenta corriente.
+1. Modificar el tipo CUENTA definido en el capítulo «Estructuras elementales» para conservar el rastro de los movimientos en una cuenta durante un mes.
+2. Establecer el saldo a final de mes de una cuenta dada.
+El saldo ya no es un atributo de la cuenta. Se obtiene recorriendo el historial mensual y anual que registra el importe del saldo de la cuenta para cada mes.
+3. Rehacer las definiciones anteriores.
+Una tabla clientes contiene las cuentas corrientes de un conjunto de clientes.
+4. Determinar los clientes para los que la media de los importes de los movimientos es superior a una suma dada."""
